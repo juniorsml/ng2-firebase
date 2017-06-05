80f79b3decfc0ad7568fc2c75160ae572094f102
valter <admin@MacBook-Pro-de-Administrador.local>
2017-06-05 08:07:52 -0300

    Course Detail

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,9 +1,10 @@
 ///<reference path="../shared/model/courses.service.ts"/>
 import { Component, OnInit } from '@angular/core';
-import {CoursesService} from "../shared/model/courses.service";
-import {Observable} from "rxjs/Observable";
-import {Lesson} from "../shared/model/lesson";
-import {ActivatedRoute} from "@angular/router";
+import {CoursesService} from '../shared/model/courses.service';
+import {Observable} from 'rxjs/Observable';
+import {Lesson} from '../shared/model/lesson';
+import {ActivatedRoute} from '@angular/router';
+import {Course} from '../shared/model/course';
 
 @Component({
   selector: 'app-course-detail',
@@ -11,15 +12,17 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./course-detail.component.css']
 })
 export class CourseDetailComponent implements OnInit {
+  course$: Observable<Course> ;
+  lessons$: Observable<Lesson[]>;
 
-  lessons$: Observable<Lesson[]>
 
   constructor(private route: ActivatedRoute , private courseService: CoursesService) { }
 
   ngOnInit() {
       const courseUrl = this.route.snapshot.params['id'];
-      console.log(courseUrl);
-      this.courseService.findLessonsForCourse(courseUrl);
+
+      this.course$ = this.courseService.findCourseByUrl(courseUrl);
+      this.lessons$ = this.courseService.findLessonsForCourse(courseUrl);
   }
 
 
--- src/app/shared/model/courses.service.ts
+++ src/app/shared/model/courses.service.ts
@@ -1,7 +1,12 @@
+///<reference path="../../../../node_modules/rxjs/add/operator/switchMap.d.ts"/>
+///<reference path="../../../../node_modules/rxjs/add/operator/map.d.ts"/>
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs/Observable";
-import {Course} from "./course";
-import {AngularFireDatabase} from "angularfire2/database";
+import {Course} from './course';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import {Lesson} from "./lesson";
+
 
 @Injectable()
 export class CoursesService {
@@ -12,7 +17,25 @@ export class CoursesService {
         return this.db.list('courses').map(Course.fromJsonList);
     }
 
-    findLessonsForCourse (courseUrl) {
+    findCourseByUrl(url) {
+        const courses$ = this.db.list('courses', { query: {
+            orderByChild : 'url',
+            equalTo : url
+        }}).map(results => results[0]);
 
+        return courses$;
+    }
+
+    findLessonsKeyPerCourse(courseURL: string): Observable<string[]> {
+        return this.findCourseByUrl(courseURL)
+            .switchMap(course => this.db.list('lessonsPerCourse/' + course.$key))
+            .map(lpcs => lpcs.map(lpc => lpc.$key));
+    }
+
+    findLessonsForCourse (courseUrl): Observable<Lesson[]> {
+
+    return this.findLessonsKeyPerCourse(courseUrl).
+        map(lskey => lskey.map(lessonKey => this.db.object('lessons/' + lessonKey)))
+        .flatMap(lsobs => Observable.combineLatest(lsobs)).do(console.log);
     }
 }
